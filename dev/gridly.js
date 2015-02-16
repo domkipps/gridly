a6839c8ebed82012e810d4640454c74ba6f46d72
domkipps <dom@dkipps.com>
2015-02-16 15:41:23 +1100

    Added 6px baseline grid

--- dev/gridly.js
+++ dev/gridly.js
@@ -6,13 +6,13 @@ var DEBUG = true;
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------
 // Dom elements
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 var $content = $('<div>') //inner col div
 	.css({
 		'height': '100%',
 		'background-color': 'pink',
 		'opacity': '0.3',
 	});
-
 
 var $col = $('<div>') //col div
 	.addClass('col-xs-1')
@@ -71,8 +71,29 @@ var $pin = $('<button>') //button
 // Functions
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------
 function removeAll() {
-	$('.js-gridly, .js-gridly-btn').remove();
+	$('.js-gridly, .js-gridly-btn, .gridlines').remove();
 	if(DEBUG) console.log('%cGridly removed!', 'color:pink;');
+}
+function addGridLines() {
+
+	var html = [];
+	var noGridLines = 1000;
+	var offsetTop = 0;
+
+	$('body').append( '<div class="gridlines" style="pointer-events: none; opacity:0; width:100%; height:100%; position:absolute; z-index:999999;"></div>' );
+
+	for (j = 0; j < noGridLines; j++)  {
+		var gridline = '<div class="gridline" style="pointer-events: none; border-bottom:1px solid rgba(255, 0, 0, 0.07); height:6px; width:100%; position:absolute; left:0; top:' + offsetTop + 'px; z-index:999999;"></div>';
+		html.push(gridline); 
+		offsetTop += 6;
+		//if(DEBUG) console.log('addGridLines');
+		if(j == noGridLines-1) {
+			$('.gridlines').append(html);
+			
+			// if(DEBUG) console.log('last gridline');
+			//if(DEBUG) console.log(html);
+		}
+	}
 }
 
 
@@ -87,6 +108,8 @@ if( $('.js-gridly').length ) { //remove
 else { //add
 
 	if(DEBUG) console.log('%cGridly created!', 'color:green;font-size:20px;');
+
+	addGridLines();
 
 	var i = 0;
 	$('.container, .container-fluid').each(function() {
@@ -126,15 +149,28 @@ else { //add
 						.on('click', function() {
 							var $id =  $(this).attr('data-id');
 							var $grid = $('.js-gridly[data-id=' + $id + ']');
+							var $gridlines = $('.gridlines');
 							var _isVisible = $grid.css('opacity') == 1;
+							var _isVisibleGridline = $gridlines.css('opacity') == 1;
 
 							$('.js-gridly').css({ //hide all open gridlies
+								'opacity': 0,
+							});
+
+							$('.body .gridline').css({ //hide all open gridlies
 								'opacity': 0,
 							});
 
 							$grid.css({ //show only this one
 								'opacity': _isVisible ? 0 : 1
 							});
+
+							$gridlines.css({ //show only this one
+								'opacity': _isVisibleGridline ? 0 : 1,
+								'top': '0px'
+							});
+
+
 						})
 
 				);
